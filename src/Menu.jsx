db4818feaf0172123ae57d348e8ff56329b1b89f
programadormovel <programadormovel@gmail.com>
2025-06-02 11:07:35 -0300

    menu

---
+++ src/Menu.jsx
@@ -0,0 +1,17 @@
+import './Menu.css'
+
+const Menu =()=>{
+
+    return (
+        <div>
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="/home">
+                        Home
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+export default Menu
